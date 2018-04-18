9180009ce50acec81f4862861af6fd76d189a594
JoeLiu <lq6414@aliyun.com>
2018-04-18 15:14:04 +0900

    delete master folder

--- unit_tests/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 3/23/18 3:26 PM
-# @Author  : Miracle Young
-# @File    : __init__.py.py
--- unit_tests/app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 4/9/18 10:29 AM
-# @Author  : Miracle Young
-# @File    : app.py
-
-from .test import fn
-
-fn()
